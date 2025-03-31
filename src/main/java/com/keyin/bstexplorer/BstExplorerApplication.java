02db49aaac87b468f50e05e5da4852fd42eb956b
kyhol <Kyle.Hollett@keyin.com>
2025-03-31 12:53:24 -0230

    Adding cors set up and BST explorer

---
+++ src/main/java/com/keyin/bstexplorer/BstExplorerApplication.java
@@ -0,0 +1,12 @@
+package com.keyin.bstexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BstExplorerApplication.class, args);
+    }
+}
